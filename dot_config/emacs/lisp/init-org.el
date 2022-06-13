2d3458457775a9b13bb010f26eca1721f2549ce1
Kyle Sexton <kyle@sexton.dev>
2022-06-13 12:20:08 -0500

    Update dot_config/emacs/lisp/init-org.el

--- dot_config/emacs/lisp/init-org.el
+++ dot_config/emacs/lisp/init-org.el
@@ -1,4 +1,14 @@
 (use-package org
   :ensure t)
 
+
+(use-package org-roam
+  :ensure t
+  :demand t  ;; Ensure org-roam is loaded by default
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/org/notes")
+  (org-roam-dailies-directory "~/org/daily"))
+
 (provide 'init-org)
